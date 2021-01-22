a0d7eb556f93185edfea1c65d358d03f3bb13e01
Bruno Santos <bm-santos@outlook.com.br>
2021-01-22 20:20:06 -0300

    components organized in folders

--- semana2/aula10/gif/src/App.js
+++ semana2/aula10/gif/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import React, {useState} from 'react'
 import './App.css';
-import Gif from './components/Gif'
+import MostraGif from './components/MostraGif'
 
 function App() {
-  const [mostrarGif, adicionaMostrarGif] = useState(false)
-  console.log(Gif.funcao)
-
-  const mostrar = () => {
-    adicionaMostrarGif(true)
-  }
-
-  const ocultar = () => {
-    adicionaMostrarGif(false)
-  }
-
+  
   return (
     <div className="App">
-      <button onClick={mostrar}>Mostrar Gif</button><br />
-      {mostrarGif && <img src="https://cdn.dicionariopopular.com/imagens/nazareconfusamatematica.gif" alt="Gif" width="200px" onClick={ocultar}/>}
+      <MostraGif />
+      
     </div>
   );
 }
